194bed004672358ed7702879f080d88510d802eb
Victor <victordelimas@gmail.com>
2022-07-19 19:58:12 -0300

    feat: config loaded fonts control

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { SignIn } from './src/screens/SignIn';
-import {NativeBaseProvider} from 'native-base';
+import {NativeBaseProvider, StatusBar} from 'native-base';
 import {THEME} from './src/styles/theme'; 
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn/>
+      <StatusBar barStyle='light-content' backgroundColor={"transparent"} translucent/>
+      {fontsLoaded ? <SignIn/> : <Loading/> }
     </NativeBaseProvider>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,13 @@
-import {View, Text } from 'react-native'
+import { VStack, Heading } from "native-base";
+
 
 export function SignIn(){
     return(
-        <View>
-            <Text>hello</Text>
-        </View>
+        <VStack flex={1} alignItems="center" bg={"gray.600"} px={8} pt={24}>
+           <Heading color={"gray.200"} fontSize="xl" mt={20} mb={6}>
+            Acesse sua conta
+           </Heading>
+        </VStack>
     )
 } 
 
