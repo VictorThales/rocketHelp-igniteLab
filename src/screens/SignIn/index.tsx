f67c0846f76505164764b1e44ad468594714510d
Victor <victordelimas@gmail.com>
2022-07-19 21:16:04 -0300

    feat: states

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,16 @@
+import { useState} from 'react';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import Logo from '../../assets/logo_primary.svg';
 import { Input } from '../../components/Input';
 import { Envelope, Key } from 'phosphor-react-native';
 import { Button } from '../../components/Button';
 
+
 export function SignIn() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
   const { colors } = useTheme();
+
   return (
     <VStack flex={1} alignItems="center" bg={'gray.600'} px={8} pt={24}>
       <Logo />
@@ -16,12 +21,14 @@ export function SignIn() {
         mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
+        onChangeText={setName}
       />
       <Input
         mb={8}
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        onChangeText={setPassword}
       />
       <Button title="Entrar" w={'full'} />
     </VStack>
